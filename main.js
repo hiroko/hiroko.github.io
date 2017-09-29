4941e6799e390808eb75bdeb8e860039db916619
Hiro <xhirokox@gmail.com>
2017-09-29 15:41:47 -0400

    Day 20

---
+++ main.js
@@ -0,0 +1,19 @@
+//note all the quotes within quotes
+
+var cnt=0, texts=["'First Quote!' - A Student", "'Second quote!' - A Colleague", "'Third Quote' - An employer"];
+
+// save the texts in an array for re-use
+$(".textContent").each(function() {
+  texts[cnt++]=$(this).text();
+});
+function slide() {
+  if (cnt>=texts.length) cnt=0;
+  $('#textMessage').html(texts[cnt++]);
+  $('#textMessage')
+    .fadeIn('slow').animate({opacity: 1.0}, 6000).fadeOut('slow',
+     function() {
+       return slide()
+     }
+  );
+}
+slide()
